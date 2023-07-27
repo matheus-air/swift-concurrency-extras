6a581b82cfa81af3a937e2c78b3bc40d62d9f120
Matheus Bispo <matheus.bispo@airapps.com>
2023-07-27 14:25:07 -0300

    feat: update minimum iOS version to 11 in Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "swift-concurrency-extras",
   platforms: [
-    .iOS(.v13),
+    .iOS(.v11),
     .macOS(.v10_15),
     .tvOS(.v13),
     .watchOS(.v6),
